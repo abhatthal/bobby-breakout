40029c5f44c67b7edebd09c8bd7b20bb7060ff8a
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-10-29 14:39:11 -0700

    Allow movement keys ghosting

--- public/scripts/user_controls.js
+++ public/scripts/user_controls.js
@@ -14,24 +14,24 @@ function keyDownHandler(event) {
     if (event.keyCode == 40 || event.keyCode == 83) {
         moveDown = true
     }
+    // Up arrow or W to move sprite up
+    else if (event.keyCode == 38 || event.keyCode == 87) {
+        moveUp = true
+    }
     // Left arrow or A for moving sprite left
-    else if (event.keyCode == 37 || event.keyCode == 65) {
+    if (event.keyCode == 37 || event.keyCode == 65) {
         moveLeft = true
     }
     // Right arrow or D to move sprite right
     else if (event.keyCode == 39 || event.keyCode == 68) {
         moveRight = true
     }
-    // Up arrow or W to move sprite up
-    else if (event.keyCode == 38 || event.keyCode == 87) {
-        moveUp = true
-    }
     // Space or E for interaction 
-    else if (event.keyCode == 32 || event.keyCode == 69) {
+    if (event.keyCode == 32 || event.keyCode == 69) {
         interactionDown = true
     }
     // Escape or P for pausing (to menu)
-    else if (event.keyCode == 27 || event.keyCode == 80) {
+    if (event.keyCode == 27 || event.keyCode == 80) {
         pauseGame = !pauseGame
     }
 }
@@ -42,20 +42,20 @@ function keyUpHandler(event) {
     if (event.keyCode == 40 || event.keyCode == 83) {
         moveDown = false
     }
+    // Up arrow or W to move sprite up
+    else if (event.keyCode == 38 || event.keyCode == 87) {
+        moveUp = false
+    }
     // Left arrow or A for moving sprite left
-    else if (event.keyCode == 37 || event.keyCode == 65) {
+    if (event.keyCode == 37 || event.keyCode == 65) {
         moveLeft = false
     }
     // Right arrow or D to move sprite right
     else if (event.keyCode == 39 || event.keyCode == 68) {
         moveRight = false
     }
-    // Up arrow or W to move sprite up
-    else if (event.keyCode == 38 || event.keyCode == 87) {
-        moveUp = false
-    }
     // Space or E for interaction 
-    else if (event.keyCode == 32 || event.keyCode == 69) {
+    if (event.keyCode == 32 || event.keyCode == 69) {
         interactionDown = false
     }
 }
