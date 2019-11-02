458229d150c23e38822f34ad1760b55943480483
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-11-01 18:18:01 -0700

    Add diagonal movement

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -29,38 +29,43 @@ container.focus();
 
 const DELTA = 5;
 
-var interactDown = false;
-var pauseGame = false;
+var keys = [];
 
 // Handles keys being pressed down
-container.addEventListener('keydown', function (event) {
+container.addEventListener('keydown', function(event) {
+	keys[event.keyCode] = true;
+
     // Down arrow or S for moving sprite down 
-    if (event.keyCode === 40 || event.keyCode === 83) {
+    if (keys[40] || keys[83]) {
         player.y = player.y + DELTA;
-    }
-    // Up arrow or W to move sprite up
-    else if (event.keyCode === 38 || event.keyCode === 87) {
+	}
+	// Up arrow or W to move sprite up
+	else if (keys[38] || keys[87]) {
         player.y = player.y - DELTA;
-    }
+	}
+	
     // Left arrow or A for moving sprite left
-    else if (event.keyCode === 37 || event.keyCode === 65) {
+    if (keys[37] || keys[65]) {
         player.x = player.x - DELTA;
     }
     // Right arrow or D to move sprite right
-    else if (event.keyCode === 39 || event.keyCode === 68) {
+    else if (keys[39] || keys[68]) {
         player.x = player.x + DELTA;
-    }
+	}
+	
     // Space or E for interaction 
-    else if (event.keyCode === 32 || event.keyCode === 69) {
+    if (keys[32] || keys[69]) {
         interactDown = true;
     }
     // Escape or P for pausing (to menu)
-    else if (event.keyCode === 27 || event.keyCode === 80) {
+    if (keys[27] || keys[80]) {
         pauseGame = !pauseGame;
-	}
-	else {
-		return;
 	}
     event.preventDefault();
     layer.batchDraw();
-})
+});
+
+// Handles when a key is released
+container.addEventListener('keyup', function(event) {
+	keys[event.keyCode] = false;
+});
