c2b77a4537340d5fea9637021f071a8e1512fbb1
Akash Bhatthal <akashbhatthal@gmail.com>
2019-11-04 12:16:42 -0800

    using switch in character.js

--- public/scripts/Character.js
+++ public/scripts/Character.js
@@ -6,27 +6,46 @@ import { Wall } from './Wall.js';
 export class Character extends Entity {
   constructor(data) {
     super(data);
-    this.speed = 5; //movement speed
+    this.speed = 5; // movement speed
   }
 
   move(dir) {
     // console.log(dir);
-    if (dir === DIRECTION.LEFT) {
-      this.x += this.speed * DIRECTION.UNIT_LEFT;
-      this.group.x(this.x);
-    }
-    else if (dir === DIRECTION.RIGHT) {
-      this.x += this.speed * DIRECTION.UNIT_RIGHT;
-      this.group.x(this.x);
-    }
+    // if (dir === DIRECTION.LEFT) {
+    //   this.x += this.speed * DIRECTION.UNIT_LEFT;
+    //   this.group.x(this.x);
+    // }
+    // else if (dir === DIRECTION.RIGHT) {
+    //   this.x += this.speed * DIRECTION.UNIT_RIGHT;
+    //   this.group.x(this.x);
+    // }
 
-    if (dir === DIRECTION.UP) {
-      this.y += this.speed * DIRECTION.UNIT_UP;
-      this.group.y(this.y);
-    }
-    else if (dir === DIRECTION.DOWN) {
-      this.y += this.speed * DIRECTION.UNIT_DOWN;
-      this.group.y(this.y);
+    // if (dir === DIRECTION.UP) {
+    //   this.y += this.speed * DIRECTION.UNIT_UP;
+    //   this.group.y(this.y);
+    // }
+    // else if (dir === DIRECTION.DOWN) {
+    //   this.y += this.speed * DIRECTION.UNIT_DOWN;
+    //   this.group.y(this.y);
+    // }
+
+    switch(dir) {
+      case DIRECTION.LEFT:
+        this.x += this.speed * DIRECTION.UNIT_LEFT;
+        this.group.x(this.x);
+        break;
+      case DIRECTION.RIGHT:
+        this.x += this.speed * DIRECTION.UNIT_RIGHT;
+        this.group.x(this.x);
+        break;
+      case DIRECTION.UP:
+        this.x += this.speed * DIRECTION.UNIT_UP;
+        this.group.x(this.x);
+        break;
+      case DIRECTION.DOWN:
+        this.x += this.speed * DIRECTION.UNIT_DOWN;
+        this.group.x(this.x);
+        break;
     }
   }
 
@@ -44,19 +63,19 @@ export class Player extends Character {
     super(data);
   }
 
-  checkCollision(obj){ //block array of Environment objects
-    if(this.isColliding(obj) ){
-      if(obj instanceof Wall){
+  checkCollision(obj){ // block array of Environment objects
+    if (this.isColliding(obj) ) {
+      if (obj instanceof Wall){
         console.log("i am touching a Wall", obj.id);
-        //bruno add the wall stuff here
-        //...
+        // bruno add the wall stuff here
+        // ...
       }
-      else if(obj instanceof NPC){
+      else if (obj instanceof NPC) {
         console.log("i am touching an NPC", obj.id);
       }
       return true;
     }
-    else{
+    else {
       return false;
     }
   }
@@ -82,7 +101,7 @@ export class NPC extends Character {
     });
   }
 
-  isSeeing(obj){
+  isSeeing(obj) {
     // console.log(this.tempRectArea);
     // console.log(this.tempRectArea.absolutePosition());
 
@@ -93,12 +112,12 @@ export class NPC extends Character {
       obj.y + obj.height < tempRectGlobalPos.y);
   }
 
-  checkPlayerDetection(player){
-    if(this.isSeeing(player) && player instanceof Player){
+  checkPlayerDetection(player) {
+    if(this.isSeeing(player) && player instanceof Player) {
       console.log("i see the player");
       return true;
     }
-    else{
+    else {
       return false;
     }
   }
