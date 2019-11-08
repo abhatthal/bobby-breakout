f63763513c9a2b9be1b1be1d608377dd64f2e297
Akash Bhatthal <akashbhatthal@gmail.com>
2019-11-08 11:12:05 -0800

    added bb-test

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => res.render('pages/login'))
 app.get('/register', (req, res) => res.render('pages/register'))
 app.get('/login', (req, res) => res.render('pages/login'))
 app.get('/main', (req, res) => res.render('pages/main'))
+app.get('/bb-test', (req, res) => res.render('pages/bb-test'))
 
 app.post('/login', (req, res) => checklogin(req, res));
 app.post('/register', (req, res) => createlogin(req, res));
