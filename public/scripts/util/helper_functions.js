669432e7ff63711a97d198b93e0c8ad1ad8d9d26
Angus Hon <angushon96@gmail.com>
2019-11-26 23:47:06 -0800

    New movement algorithm
    -collision detection happens before actually moving
    -next position / move is simulated and tested to see if there's a collision
    -if collision, then no movement will be made.
    -if no collision, movement will be made
    -can only move in one direction at a time

--- public/scripts/util/helper_functions.js
+++ public/scripts/util/helper_functions.js
@@ -11,6 +11,15 @@ export function genID() {
   return id;
 }
 
+export function isColliding(obj1, obj2) {
+  console.assert(obj1 != null);
+  console.assert(obj2 != null);
+  return !(obj1.x > obj2.x + obj2.width ||
+       obj1.x + obj1.width < obj2.x ||
+       obj1.y > obj2.y + obj2.height ||
+       obj1.y + obj1.height < obj2.y);
+}
+
 export const DIRECTION = {
   LEFT: 'LEFT',
   RIGHT: 'RIGHT',
@@ -21,6 +30,20 @@ export const DIRECTION = {
   UNIT_UP: 1,
   UNIT_DOWN: -1,
 };
+
+export function oppositeDirection(dir) {
+  let oppDir;
+  if (dir === DIRECTION.RIGHT) {
+    oppDir = DIRECTION.LEFT;
+  } else if (dir === DIRECTION.LEFT) {
+    oppDir = DIRECTION.RIGHT;
+  } else if (dir === DIRECTION.UP) {
+    oppDir = DIRECTION.DOWN;
+  } else if (dir === DIRECTION.DOWN) {
+    oppDir = DIRECTION.UP;
+  }
+  return oppDir;
+}
 
 export async function loadImage(imageUrl) {
   let img;
