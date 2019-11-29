f0ac593192379b4120b0cfc63ab429b2e79f3bc9
Angus Hon <angushon96@gmail.com>
2019-11-29 06:16:53 -0800

    Included preprocessing files for map, removed some legacy debug code
    -bounding box still not working, could use feedback

--- public/scripts/world/MapASB.js
+++ public/scripts/world/MapASB.js
@@ -1,8 +1,8 @@
 import {WallLine} from './WallLine.js';
 import {NPC} from './Character.js';
-import {Environment} from './Environment.js'; // for spawn and end points?
-import {csil_coords} from '../../assets/csil_coords.js';
-import {csil_labs} from '../../assets/csil_labs.js';
+// import {Environment} from './Environment.js'; // for spawn and end points?
+import {CsilCoords} from '../../assets/csil_coords.js';
+// import {CsilLabs} from '../../assets/csil_labs.js';
 
 export class MapASB {
   constructor(data) {
@@ -12,30 +12,19 @@ export class MapASB {
     const displacement = {
       x: (stage.width()/2 - 30),
       y: (stage.height()/2 - 30),
-    }
+      // x: -665,
+      // y: -3170,
+    };
 
     /*
-    TEMP: 
-    - turn points -> single array of points [x1, y1, x2, y2, x3, y3] 
-    - not array of pairs, for now to test have points1 = to that
-    
-    TODO: 
-    - map too small, scale all coords by a factor of like 4? or maybe 5? quickly do it in python then rexport?
-    - make new bounding box class that returns a bounding box for each line rather than the whole line obj as a whole
-    - probably for loop the new line bounding box class to create it for the whole thing
+    ISSUE:
+    -bounding boxes not triggering actual collision since only a signle instance of WallLine is made
+    -WallLine points all clustered around displacement value
+    -should convert WallLine class to make new lines based on array of points passed in
+    -rn its just drawing the cluster of points
+    -can do similar method like in LineBoundingBox to split the array of data into pairs
+    -set the Line's x,y = (x1,y1)? each Line is an (x1,y1) (x2,y2)
     */
-
-    // const wall1 = new WallLine({
-    //   x: displacement.x,
-    //   y: displacement.y,
-    //   points: pathItem.points,
-    //   colour: 'black',
-    //   width: 10,
-    //   height: 0,
-    //   name: pathItem.name,
-    //   impassible: true,
-    // });
-    // layer.add(wall1.render);
 
     const npc = new NPC({
       x: 400,
@@ -56,10 +45,10 @@ export class MapASB {
     this.blockArray = [];
     // console.log(this.blockArray)
 
-    for(let i = 0; i < csil_coords.length; i++){
-      let curr = csil_coords[i];
+    for (let i = 0; i < CsilCoords.length; i++) {
+      const curr = CsilCoords[i];
       // console.log(curr);
-      let currLine = new WallLine({
+      const currLine = new WallLine({
         x: displacement.x,
         y: displacement.y,
         points: curr.points,
@@ -72,15 +61,15 @@ export class MapASB {
       this.blockArray.push(currLine);
     }
 
-    for(let i = 0; i < this.blockArray.length; i++){
-      let curr = this.blockArray[i];
+    for (let i = 0; i < this.blockArray.length; i++) {
+      const curr = this.blockArray[i];
       layer.add(curr.render);
     }
 
     // #region text on map
     // this.textArray = [];
-    // for(let i = 0; i < csil_labs.length; i++){
-    //   let curr = csil_labs[i];
+    // for(let i = 0; i < CsilLabs.length; i++){
+    //   let curr = CsilLabs[i];
     //   let currText =  new Konva.Text({
     //     x: curr.points[0],
     //     y: curr.points[1],
